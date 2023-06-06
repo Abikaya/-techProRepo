25e03c35df9cd3b6d8bdc78f90a28648acf88c38
Abikaya <aysegul.bicinkaya@hotmail.de>
2023-06-06 18:35:07 +0200

    Version 1

---
+++ src/main/java/DemoClass.java
@@ -0,0 +1,8 @@
+public class DemoClass {
+    public static void main(String[] args) {
+        int x=1;
+        int y=2;
+        System.out.println((x+y));
+        System.out.println("Versiyon 1 icin islemler tamamlandi");
+    }
+}
